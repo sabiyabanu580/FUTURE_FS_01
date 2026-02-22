d8b65c2ad3883ea2bdff2241aa635fe74c2a42d5
sabiyabanu580 <sabiyabanu316@gmail.com>
2026-02-22 13:26:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# FUTURE_FS_01
+
+  # Personal Portfolio Website
+
+  This is a code bundle for Personal Portfolio Website. The original project is available at https://www.figma.com/design/YZxncsSdCbCLkMPjPTblAX/Personal-Portfolio-Website.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    // The React and Tailwind plugins are both required
+    // even if Tailwind is not actively used
+    react(),
+    tailwindcss(),
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  // File types to support raw imports
+  assetsInclude: ["**/*.svg", "**/*.csv"],
+});
